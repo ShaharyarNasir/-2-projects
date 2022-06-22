e6ec1f668a09105623b004733faacfff5f81e6fe
M Shaharyar Nasir <mshaharyarnasir@gmail.com>
2022-06-22 21:54:13 +0500

    Python Projects

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+# youtuber = "Sherry"
+
+# print("Subscribe to " + youtuber)
+# print("Subscribe to {}".format(youtuber))
+# print(f"Subscribe to {youtuber}")
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous Person: ")
+
+medlib = f"Computer programing is so {adj}! It makes me so excited all the time because I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(medlib)
