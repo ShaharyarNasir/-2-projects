76b08401639d5b0812047270f6d6b5db4c73403e
M Shaharyar Nasir <mshaharyarnasir@gmail.com>
2022-06-22 23:01:39 +0500

    medlibs complete

--- madlibs.py
+++ madlibs.py
@@ -11,3 +11,4 @@ famous_person = input("Famous Person: ")
 medlib = f"Computer programing is so {adj}! It makes me so excited all the time because I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
 
 print(medlib)
+ 
